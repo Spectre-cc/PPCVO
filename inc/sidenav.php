2fade2070151d24da7b450c36c6a1e624395c313
Spectre-cc <duenasmauie@gmail.com>
2022-10-15 18:00:04 +0800

    inc folder

---
+++ inc/sidenav.php
@@ -0,0 +1,16 @@
+            <!-- Sidebar -->
+            <div class="sidenav container-fluid text-center col-2 h-auto pt-4" style="height: 100%;">
+                <div class="container text-center">
+                    <img class="sidebar-logo img-fluid" src="style/assets/images/PPCVIO-Logo-wht.png" alt="" >
+                </div>
+                <div class="container list-group text-start px-4">
+                    <hr class="horizontal-rule p-0 mt-2 mb-2">
+                    <a href="" class="navlinks link-light mt-2 mb-2"><i class="fa-solid fa-chart-line"></i> Analytics</a>
+                    <a href="" class="navlinks link-light mb-2"><i class="fa-solid fa-address-book"></i> Clients</a>
+                    <a href="" class="navlinks link-light"><i class="fa-solid fa-paw"></i> Animal Records</a>
+                    <hr class="horizontal-rule p-0 mt-2 mb-2">
+                    <a href="" class="navlinks link-light"><i class="fa-solid fa-user"></i> Users</a>
+                    <a href="" class="navlinks link-light mt-2"><i class="fa-solid fa-right-from-bracket"></i> Log Out</a>
+                </div>
+            </div>
+            <!-- Sidebar -->
