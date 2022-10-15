2c610eb306b0c4a1d80f6021f495dcf2a44f0048
Spectre-cc <duenasmauie@gmail.com>
2022-10-15 19:55:18 +0800

    additional page (user) and sidenav pointers

--- View-Users.php
+++ View-Users.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <?php require('inc\links.php'); ?>
-    <title>Animals Owned</title>
+    <title>Users</title>
 </head>
 <body>
     <div class="container-fluid m-0 p-0">
@@ -13,29 +13,17 @@
                     <div class="container pt-4">
                         <div class="container text-start px-1">
                             <div class="container-fluid text-center">
-                                <h2>Animals owned by John Doe</h2>
-                            </div>
-                            <div class="container-fluid mb-2">
-                                <form action="">
-                                    <div class="form-group d-flex justify-content-center">
-                                        <input class="form-control inputbox w-25 mx-1" type="search" name="search" id="search" placeholder="Search client name...">
-                                        <input class="btn btn-primary" type="submit" id="submit" value="Search">
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="container-fluid text-center mb-2">
-                                <button class="btn btn-success"><i class="fa-solid fa-plus"></i> Add</button>
+                                <h2>Users</h2>
                             </div>
                             <div class="container-fluid d-flex justify-content-start align-items-start overflow-scroll">
                                 <table class="table table-condensed table-bordered table-hover table-responsive text-start">
                                     <thead>
                                         <th class="medcell text-bg-dark">Name</th>
-                                        <th class="medcell text-bg-dark">Species</th>
-                                        <th class="medcell text-bg-dark">Breed</th>
-                                        <th class="medcell text-bg-dark">Color</th>
-                                        <th class="smallcell text-bg-dark">Sex</th>
-                                        <th class="smallcell text-bg-dark">Age</th>
-                                        <th class="autocell bg-dark"></th>
+                                        <th class="medcell text-bg-dark">Email</th>
+                                        <th class="medcell text-bg-dark">Password</th>
+                                        <th class="largecell text-bg-dark">Contact Number</th>
+                                        <th class="largecell text-bg-dark">Address</th>
+                                        <th class="medcell bg-dark"></th>
                                     </thead>
                                     <tbody>
                                         <tr>
@@ -44,10 +32,8 @@
                                             <td class="medcell">Persian</td>
                                             <td class="medcell">Black</td>
                                             <td class="smallcell">Male</td>
-                                            <td class="smallcell">3 months</td>
-                                            <td class="autocell d-flex justify-content-center align-items-center" >
+                                            <td class="med d-flex justify-content-center align-items-center" >
                                                 <button class="btn btn-primary mx-1"><i class="fa-solid fa-pen-to-square"></i> Update</button> 
-                                                <button class="btn btn-primary mx-1"><i class="fa-solid fa-notes-medical"></i> View Health History</button> 
                                                 <button class="btn btn-danger mx-1"><i class="fa-solid fa-trash"></i></button>
                                             </td>
                                         </tr>
