aac785bcbb6d9243e6c63e52e80c5306c6dae25b
Spectre-cc <duenasmauie@gmail.com>
2022-11-08 15:19:48 +0800

    Dropdown menu

--- inc/sidenav-admin.php
+++ inc/sidenav-admin.php
@@ -6,8 +6,15 @@
                 <div class="container list-group text-center px-2">
                 <hr class="horizontal-rule p-0 my-3">
                     <a href="Analytics.php" class="btn btn-light bg-transparent text-white my-1"><i class="fa-solid fa-chart-line"></i> Analytics</a>
-                    <a href="View-Users-Personnel.php" class="btn btn-light bg-transparent text-white my-1"><i class="fa-solid fa-person"></i> Personnel</a>
-                    <a href="View-Users-Admin.php" class="btn btn-light bg-transparent text-white my-1"><i class="fa-solid fa-user-tie"></i> Admin</a>
+                    <div class="dropdown">
+                        <button class="btn btn-light bg-transparent text-white my-1 w-100 dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fa-solid fa-users"></i> Users
+                        </button>
+                        <ul class="text-center dropdown-menu">
+                            <li><a class="dropdown-item" href="View-Users-Personnel.php"><i class="fa-solid fa-person"></i> Personnel</a></li>
+                            <li><a class="dropdown-item" href="View-Users-Admin.php"><i class="fa-solid fa-user-tie"></i> Admin</a></li>
+                        </ul>
+                    </div>
                     <hr class="horizontal-rule p-0 my-3">
                     <a href="functions/logout.php" class="btn btn-light bg-transparent text-white my-1"><i class="fa-solid fa-right-from-bracket"></i> Log Out</a>
                 </div>
