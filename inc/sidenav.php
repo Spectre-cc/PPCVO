e0e8bec72c4c482e03f84662f9201e71ab166822
Spectre-cc <duenasmauie@gmail.com>
2022-11-26 16:39:48 +0800

    sidenav modification

--- inc/sidenav.php
+++ inc/sidenav.php
@@ -4,7 +4,7 @@
                     <img class="sidebar-logo img-fluid" src="inc\style\assets\images\PPCVIO-Logo-wht.png" alt="" >
                 </div>
                 <div class="container list-group text-center px-2 ">
-                    <a href="View-User-Account.php" class="link-light p-0 mt-3" style="text-decoration: none;"><i class="fa-solid fa-user"></i> Good day, <?php echo $_SESSION['fName']?></a>
+                    <a href="View-User-Account.php" class="btn btn-outline-light border-0 rounded-pill p-0 mt-3" style="text-decoration: none;"><i class="fa-solid fa-user"></i> Good day, <?php echo $_SESSION['fName']?></a>
                     <hr class="horizontal-rule p-0 mb-3">
                     <a href="View-Client-List.php" class="btn btn-outline-light mb-2 rounded-pill shadow"><i class="fa-solid fa-address-book"></i> Clients</a>
                     <a href="Send-Email.php" class="btn btn-outline-light  mb-2 rounded-pill shadow"><i class="fa-solid fa-envelope"></i> Send Announcement</a>
